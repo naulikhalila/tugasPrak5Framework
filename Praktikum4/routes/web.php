b2d7ed9003763ebb866155e319f47789a3c359d4
naulikhalila <151435542+naulikhalila@users.noreply.github.com>
2024-04-03 14:43:05 +0700

    Add files via upload

---
+++ Praktikum4/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('home', [HomeController::class, 'index'])->name('home');
+Route::get('profile', ProfileController::class)->name('profile'); 
+Route::resource('employees', EmployeeController::class);
